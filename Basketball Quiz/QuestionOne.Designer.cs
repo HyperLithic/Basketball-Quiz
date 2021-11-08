a4eaa81f05aed5fcdc7d82ba0e824863133a6181
HyperLithic <godnmaste19@gmail.com>
2021-11-08 13:00:11 +1300

    center button

--- Basketball Quiz/QuestionOne.Designer.cs
+++ Basketball Quiz/QuestionOne.Designer.cs
@@ -28,34 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QuestionOne));
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // button1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(450, 186);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Location = new System.Drawing.Point(65, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(655, 365);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             // 
             // QuestionOne
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 461);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Name = "QuestionOne";
             this.Text = "QuestionOne";
             this.Load += new System.EventHandler(this.QuestionOne_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Basketball Quiz/QuestionOne.cs
+++ Basketball Quiz/QuestionOne.cs
@@ -16,10 +16,25 @@ namespace Basketball_Quiz
         {
             InitializeComponent();
         }
-
-        private void QuestionOne_Load(object sender, EventArgs e)
+        Button btnStartOne = new Button();
+        public void QuestionOne_Load(object sender, EventArgs e)
         {
+            //i have got the height of the form and taken away the size of the button and divided by 2 to get the difference and center the button, 
+            //and then adjusted to get measurements better because it seems the measurements are not entirely accurate unfortately
+            btnStartOne.SetBounds((this.Width - (655 + ((this.Width) / 100) * 7)), (this.Height - (365 + ((this.Height) / 100) * 7)), 655, 365);
+            Console.WriteLine(this.Width);
+            Console.WriteLine(this.Height);
+            this.Controls.Add(btnStartOne);
+            btnStartOne.Text = "";
+            btnStartOne.BackgroundImage = Image.FromFile("duo logo.png");
+            btnStartOne.Click += new EventHandler(this.btnStartOne_Click);
+            //this.btnStartOneFake.Visible = false;
+        }
 
+        void btnStartOne_Click(object sender, EventArgs e)
+        {
+            //Hide the button clicked
+            btnStartOne.Hide();
         }
     }
 }
