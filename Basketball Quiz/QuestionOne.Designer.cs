ab8ca6ca0fe2f23782d9a5fc654517ec0ecd734b
HyperLithic <65694586+HyperLithic@users.noreply.github.com>
2021-11-09 20:06:50 +1300

    Add files via upload

--- Basketball Quiz/QuestionOne.Designer.cs
+++ Basketball Quiz/QuestionOne.Designer.cs
@@ -30,23 +30,71 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QuestionOne));
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
-            this.button1.Location = new System.Drawing.Point(65, 50);
+            this.button1.Location = new System.Drawing.Point(45, 320);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(655, 365);
+            this.button1.Size = new System.Drawing.Size(330, 110);
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.Location = new System.Drawing.Point(415, 320);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(330, 110);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.Location = new System.Drawing.Point(45, 190);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(330, 110);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.Location = new System.Drawing.Point(415, 190);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(330, 110);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+            this.button5.Location = new System.Drawing.Point(45, 35);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(110, 110);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
             // 
             // QuestionOne
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "QuestionOne";
             this.Text = "QuestionOne";
@@ -58,5 +106,9 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
--- Basketball Quiz/QuestionOne.cs
+++ Basketball Quiz/QuestionOne.cs
@@ -17,16 +17,21 @@ namespace Basketball_Quiz
             InitializeComponent();
         }
         Button btnStartOne = new Button();
+        Button[] btnAnswer = new Button[4];
+        //Button btnAnswerOne = new Button();
+        //Button btnAnswerTwo = new Button();
+        //Button btnAnswerThree = new Button();
+        //Button btnAnswerFour = new Button();
         public void QuestionOne_Load(object sender, EventArgs e)
         {
             //i have got the height of the form and taken away the size of the button and divided by 2 to get the difference and center the button, 
             //and then adjusted to get measurements better because it seems the measurements are not entirely accurate unfortately
-            btnStartOne.SetBounds((this.Width - (655 + ((this.Width) / 100) * 7)), (this.Height - (365 + ((this.Height) / 100) * 7)), 655, 365);
+            btnStartOne.SetBounds((this.Width - 670) / 2, (this.Height - 403) / 2, 655, 365);
             Console.WriteLine(this.Width);
             Console.WriteLine(this.Height);
             this.Controls.Add(btnStartOne);
             btnStartOne.Text = "";
-            btnStartOne.BackgroundImage = Image.FromFile("duo logo.png");
+            //btnStartOne.BackgroundImage = Image.FromFile("duo logo.png");
             btnStartOne.Click += new EventHandler(this.btnStartOne_Click);
             //this.btnStartOneFake.Visible = false;
         }
